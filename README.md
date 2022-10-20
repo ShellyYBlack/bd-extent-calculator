a51157cf3160209da906048d072426263006a0b4
ShellyYBlack <syblack@ncsu.edu>
2022-10-19 21:41:00 -0400

    Moved functions to one file

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # bd-extent-calculator
 
-This script calculates the total extent in MB and files from the quantity and unittype tags in an EAD finding aid. It provides the totals for the entire collection and each series, as well as all the collections.
+This script calculates the total extent in MB and files from the quantity and unittype tags in an EAD XML finding aid. It provides the totals for the entire collection and each series, as well as all the collections.
 
 It has not been tested on EAD3.
 
 1. Install Beautiful Soup: `sudo apt-get install python3-bs4`
 2. Downloaded list of finding aid URLs: `wget https://www.lib.ncsu.edu/findingaids/ead.txt`
 3. Downloaded all finding aids: `download-EAD.sh`
-3. Calculate extents by replacing path to finding aids and running: `python3 bd-extent-calculator-multi.py /path/to/ead/file/or/dir > output.csv`
+3. Calculate extents by replacing path to directory and running: `python3 bd-extent-calculator-multi.py /path/to/ead/file/or/dir > output.csv`
+
+You can also run this script on a single EAD XML file.
--- bd-extent-calculator.py
+++ bd-extent-calculator.py
@@ -1,12 +1,100 @@
 from bs4 import BeautifulSoup
-from parse_collection import parse_collection
-import sys
+import sys,os,glob,re
 
+# This parses the series
+def parse_series(seriesTitle, collectionTitle):
+    did = seriesTitle.parent
+
+    # Create a list of c tags or file records
+    listFileRecords =[]
+    for sibling in did.next_siblings:
+        listFileRecords.append(sibling)
+
+    # Create a list of all the quantity tags
+    listQuantity =[]
+    for c in listFileRecords:
+        for quantity in c.findAll('quantity'):
+            listQuantity.append(quantity)
+
+    # For each quantity tag, if its next sibling (unittype tag) has '...bytes' or 'Files' add to list
+    listKB = []
+    listMB = []
+    listGB = []
+    listFiles =[]
+    for quantity in listQuantity:
+        intQuantity = eval(quantity.text)
+        if re.search('[kK]ilobytes?', quantity.next_sibling.text):
+            listKB.append(intQuantity)
+        if re.search('[mM]egabytes?', quantity.next_sibling.text):
+            listMB.append(intQuantity)
+        if re.search('[gG]igabytes?', quantity.next_sibling.text):
+            listGB.append(intQuantity)
+        if re.search('[fF]iles?', quantity.next_sibling.text):
+            listFiles.append(intQuantity)
+
+    # Get a sum total of the lists
+    totalKB = sum(listKB)
+    totalMB = sum(listMB)
+    totalGB = sum(listGB)
+    seriesTotalFiles = sum(listFiles)
+
+    # Add up the lists of bytes and files to get grand totals
+    seriesTotalMB = totalKB/1024+totalMB+totalGB*1024
+    seriesResult = '"' + collectionTitle + '/' + seriesTitle.text + '"' + ',' + str(round(seriesTotalMB)) + ',' + str(seriesTotalFiles)
+    return [seriesTotalMB,seriesTotalFiles,seriesResult]
+
+# This parses the collection
+def parse_collection(soup):
+
+    collectionTotalSizeMB = 0
+    collectionTotalFiles = 0
+    collectionTitle = soup.select('archdesc > did unittitle')[0].text
+    seriesResultList = []
+    
+    # Get series titles
+    seriesTitles = soup.select('dsc > c > did >  unittitle')
+    for seriesTitle in seriesTitles:
+        seriesTotalMB, seriesTotalFiles, seriesResult = parse_series(seriesTitle,collectionTitle)
+        collectionTotalSizeMB = collectionTotalSizeMB + seriesTotalMB
+        collectionTotalFiles = collectionTotalFiles + seriesTotalFiles
+        seriesResultList.append(seriesResult)
+    
+    print('"' + collectionTitle + '",' + str(round(collectionTotalSizeMB)) + ',' + str(collectionTotalFiles))
+    for seriesResult in seriesResultList:
+        print(seriesResult)
+        
+    return [collectionTotalSizeMB,collectionTotalFiles]
+
+# Main script
 scriptName, path = sys.argv
+parseOneEAD = path.endswith('.xml')
 
-with open(path, 'r') as f:
-    file = f.read()
+print("Title,MB,Files")
 
-soup = BeautifulSoup(file, 'xml')
-parse_collection(soup)
+if parseOneEAD:
+    with open(path, 'r') as f:
+        file = f.read()
 
+    soup = BeautifulSoup(file, 'xml')
+    parse_collection(soup)
+else:
+    collectionsTotalSizeMB = 0
+    collectionsTotalFiles = 0
+    numberCollections = 0
+    fileNameList = glob.glob(os.path.join(path,'*.xml'))
+
+    if len(fileNameList) == 0:
+        print("There were no XML files in this directory.")
+        sys.exit()
+
+    # Calling each XML file from path to directory
+    for filename in fileNameList:
+        with open(filename, 'r') as f:
+            file = f.read()
+
+        soup = BeautifulSoup(file, 'xml')
+        grandTotalSizeMB, grandTotalFiles = parse_collection(soup)
+        collectionsTotalSizeMB = collectionsTotalSizeMB + grandTotalSizeMB
+        collectionsTotalFiles = collectionsTotalFiles + grandTotalFiles
+        numberCollections = numberCollections + 1
+    print('"Total for ' + str(numberCollections) + ' collections",' + str(round(collectionsTotalSizeMB)) + ',' + str(collectionsTotalFiles))
