d60f12c42caac84fa78a67a6b24ecce77a90d2ce
ShellyYBlack <syblack@ncsu.edu>
2022-10-20 09:23:08 -0400

    Update README

--- download-EAD.sh
+++ download-EAD.sh
@@ -3,4 +3,4 @@ URLpath=${line#https://www.lib.ncsu.edu/findingaids/}
 NAME=${URLpath%/ead}.xml
 echo "$line"
 wget -O $NAME "$line"   
-done < /Users/borndigital/born_digital/ead.txt
+done < ead.txt
